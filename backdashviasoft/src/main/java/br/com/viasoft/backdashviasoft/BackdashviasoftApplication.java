79a673845e0ab465b4c02ee08f9ea5ca71094ca4
MarcioTesser <marciortesser@gmail.com>
2020-10-07 13:44:39 -0300

    Primeiro commit

---
+++ backdashviasoft/src/main/java/br/com/viasoft/backdashviasoft/BackdashviasoftApplication.java
@@ -0,0 +1,13 @@
+package br.com.viasoft.backdashviasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackdashviasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackdashviasoftApplication.class, args);
+	}
+
+}
